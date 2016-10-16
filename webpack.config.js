33193ea499a557a13d1b5a0a5537f5cfce76ab12
debrawatkins <debwatkins2@gmail.com>
2016-10-15 17:02:09 -0700

    completed reveal on scroll feature

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,16 @@ module.exports = {
     path: "./app/temp/scripts",
     filename: "App.js"
   },
-
   module: {
-  	loaders: [
-  	{
-  		loader: 'babel',
-  		query: {
-  			presets: ['es2015']
-  		},
-  		test: /\.js$/,
-  		exclude: /node_modules/ 
-  	 }
-  	]
+    loaders: [
+      {
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        },
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
   }
 }
